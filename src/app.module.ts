bd4d17ec1f09e8699a9ac0efe9a23d304914dc42
fkaaziebu <frederickaziebu1998@gmail.com>
2025-04-11 01:14:32 +0000

    update redis connection object

--- src/app.module.ts
+++ src/app.module.ts
@@ -46,10 +46,7 @@ import { FarmModule } from "./farm/farm.module";
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         connection: {
-          host: configService.get("REDIS_HOST", "localhost"),
-          port: configService.get("REDIS_PORT", 6379),
-          password: configService.get("REDIS_PASSWORD", undefined),
-          db: configService.get("REDIS_DB", 0),
+          url: configService.get<string>("REDIS_URL"),
         },
       }),
     }),
